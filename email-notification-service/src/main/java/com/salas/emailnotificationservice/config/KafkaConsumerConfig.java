fbcfce0be23406121fbccf3ea8bca84ac603bf71
Vladyslav Hasporian <saslasengland33@gmail.com>
2025-02-26 23:23:39 +0200

    add errorHandler and create topic with strategy 'dead latter topic' for brokeEvent(can't deserialize)

--- email-notification-service/src/main/java/com/salas/emailnotificationservice/config/KafkaConsumerConfig.java
+++ email-notification-service/src/main/java/com/salas/emailnotificationservice/config/KafkaConsumerConfig.java
@@ -2,15 +2,20 @@ package com.salas.emailnotificationservice.config;
 
 import com.salas.common.ProductCreatedEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.List;
@@ -23,6 +28,8 @@ public class KafkaConsumerConfig {
     private List<String> bootstrapServers;
     @Value("${spring.kafka.consumer.group-id}")
     private String groupId;
+    @Value("${spring.kafka.consumer.properties.spring.json.trusted.packages}")
+    private String trustedPackage;
 
     @Bean
     public ConsumerFactory<String, ProductCreatedEvent> consumerFactory() {
@@ -30,20 +37,40 @@ public class KafkaConsumerConfig {
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, String.join(",", bootstrapServers));
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, JsonDeserializer.class);
+
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, trustedPackage);
         return new DefaultKafkaConsumerFactory<>(
-                props, new StringDeserializer(), new JsonDeserializer<>(ProductCreatedEvent.class)
+                props, new StringDeserializer(), new ErrorHandlingDeserializer<>(new JsonDeserializer<>(ProductCreatedEvent.class))
         );
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, ProductCreatedEvent>
-    kafkaListenerContainerFactory() {
+    kafkaListenerContainerFactory(KafkaTemplate<String, Object> kafkaTemplate) {
+
+        DefaultErrorHandler defaultErrorHandler = new DefaultErrorHandler(new DeadLetterPublishingRecoverer(kafkaTemplate));
 
         ConcurrentKafkaListenerContainerFactory<String, ProductCreatedEvent> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
 
         factory.setConsumerFactory(consumerFactory());
+        factory.setCommonErrorHandler(defaultErrorHandler);
         return factory;
     }
+
+    @Bean
+    KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+    @Bean
+    ProducerFactory<String, Object> producerFactory() {
+        HashMap<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, String.join(",", bootstrapServers));
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
+    };
 }
